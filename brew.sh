963a872eac21305414f15aec19cf8c9505b122b8
Matthew <matthewh806@gmail.com>
2016-04-24 20:38:23 +0200

    Added a script for installing common homebrew formulae

---
+++ brew.sh
@@ -0,0 +1,34 @@
+#/usr/bin/env bash
+
+# Install command-line tools using homebrew
+
+# Use the latest homebrew
+brew update
+
+# Upragde existing formulae
+brew upgrade --all
+
+brew install ant
+brew install cask
+brew install emacs
+brew install cmake
+brew install duck
+brew install cscope
+brew install ffmpeg
+brew install lame
+brew install makedepend
+brew install macvim
+brew install node
+brew install openssl
+brew install pkg-config
+brew install unrar
+brew install wget
+brew install xvid
+brew install x264
+brew install xz
+
+# Remove outdated versions
+brew cleanup
+
+
+
